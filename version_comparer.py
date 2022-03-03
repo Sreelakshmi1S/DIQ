38e5e14ac027bc8ffcf371f557b0e13b35718eba
Sreelakshmi S <sreelakshmiponakam@gmail.com>
2022-03-03 13:52:11 +0530

    Create version_comparer.py

---
+++ version_comparer.py
@@ -0,0 +1,71 @@
+"""Input: 
+version1 = "1.0.33"
+version2 = "1.0.27"
+Output: 1 
+#version1 > version2
+
+Input:
+version1 = "0.1"
+version2 = "1.1"
+Output: -1
+#version1 < version2
+
+Input: 
+version1 = "1.01"
+version2 = "1.001"
+Output: 0
+#ignore leading zeroes, 01 and 001 represent the same number. 
+
+Input:
+version1 = "1.0"
+version2 = "1.0.0"
+Output: 0
+#version1 does not have a 3rd level revision number, which
+defaults to "0" """
+
+
+
+
+
+
+def versionCompare(v1, v2):
+     
+    # This will split both the versions by '.'
+    arr1 = v1.split(".")
+    arr2 = v2.split(".")
+    n = len(arr1)
+    m = len(arr2)
+     
+    # converts to integer from string
+    arr1 = [int(i) for i in arr1]
+    arr2 = [int(i) for i in arr2]
+    print(arr1)
+    print(arr2)
+    # compares which list is bigger and fills
+    # smaller list with zero (for unequal delimeters)
+    if n>m:
+      for i in range(m, n):
+         arr2.append(0)
+    elif m>n:
+      for i in range(n, m):
+         arr1.append(0)
+        
+    
+    # returns 1 if version 1 is bigger and -1 if
+    # version 2 is bigger and 0 if equal
+    print(arr1)
+    print(arr2)
+    
+    for i in range(len(arr1)):
+      if arr1[i]>arr2[i]:
+         return 1
+      elif arr2[i]>arr1[i]:
+         return -1
+    return 0
+ 
+# Driver program to check above comparison function
+version1 = "1.0.0"
+version2 = "1.0"
+ 
+ans = versionCompare(version1, version2)
+print(ans)
