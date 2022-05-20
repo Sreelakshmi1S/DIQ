15a674fc94a0d2eb9d9733daef02a64ac997cee1
Sreelakshmi S <sreelakshmiponakam@gmail.com>
2022-05-20 22:50:00 +0530

    Update closest_points.py

--- closest_points.py
+++ closest_points.py
@@ -10,3 +10,14 @@ print(closest_points([(0, 0), (1, 2), (-3, 4), (3, 1)], 2))
 
 
 def pClosest(points, K):
+ 
+    points.sort(key = lambda K: K[0]**2 + K[1]**2)
+ 
+    return points[:K]
+ 
+# Driver program
+points = [[3, 3], [5, -1], [-2, 4]]
+ 
+K = 2
+ 
+print(pClosest(points, K))
